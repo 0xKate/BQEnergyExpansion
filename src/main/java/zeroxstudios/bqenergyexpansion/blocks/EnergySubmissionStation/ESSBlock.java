5e45b413e62af2390fe64cee7b6074f2a15e1e11
0xKate <94711905+0xKate@users.noreply.github.com>
2022-09-10 14:05:20 -0600

    New Block Texture

--- src/main/java/zeroxstudios/bqenergyexpansion/blocks/EnergySubmissionStation/ESSBlock.java
+++ src/main/java/zeroxstudios/bqenergyexpansion/blocks/EnergySubmissionStation/ESSBlock.java
@@ -1,14 +1,18 @@
 package zeroxstudios.bqenergyexpansion.blocks.EnergySubmissionStation;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import java.util.ArrayList;
 import java.util.Random;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 import zeroxstudios.bqenergyexpansion.core.BQEnergyExpansion;
 import zeroxstudios.bqenergyexpansion.core.Tags;
@@ -17,11 +21,21 @@ public class ESSBlock extends BlockContainer {
 
     public static final String name = "energy_submission_station";
     private final Random rng = new Random();
+    private IIcon topIcon;
+    private final IIcon[] sideIcons = new IIcon[15];
+    private int currentIcon = 0;
+
+    public boolean setIcon(int n) {
+        boolean changed = !(this.currentIcon == n);
+        this.currentIcon = n;
+        return changed;
+    }
 
     public ESSBlock() {
         super(Material.iron);
+        this.setHardness(1);
         this.setBlockName(Tags.MODID + "." + name);
-        this.setBlockTextureName(Tags.MODID + ":" + name);
+        this.setBlockTextureName(Tags.MODID + ":" + "ess");
     }
 
     public String getName() {
@@ -65,5 +79,34 @@ public class ESSBlock extends BlockContainer {
     @Override
     public TileEntity createNewTileEntity(World w, int p_149915_2_) {
         return new ESSTileEntity();
+    }
+
+    @SideOnly(Side.CLIENT)
+    public IIcon getIcon(int side, int meta) {
+        if (side == 0 || side == 1) {
+            return topIcon;
+        } else {
+            return sideIcons[currentIcon];
+        }
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister iconRegistry) {
+        this.sideIcons[0] = iconRegistry.registerIcon(this.getTextureName() + "_side_0");
+        this.sideIcons[1] = iconRegistry.registerIcon(this.getTextureName() + "_side_1");
+        this.sideIcons[2] = iconRegistry.registerIcon(this.getTextureName() + "_side_2");
+        this.sideIcons[3] = iconRegistry.registerIcon(this.getTextureName() + "_side_3");
+        this.sideIcons[4] = iconRegistry.registerIcon(this.getTextureName() + "_side_4");
+        this.sideIcons[5] = iconRegistry.registerIcon(this.getTextureName() + "_side_5");
+        this.sideIcons[6] = iconRegistry.registerIcon(this.getTextureName() + "_side_6");
+        this.sideIcons[7] = iconRegistry.registerIcon(this.getTextureName() + "_side_7");
+        this.sideIcons[8] = iconRegistry.registerIcon(this.getTextureName() + "_side_8");
+        this.sideIcons[9] = iconRegistry.registerIcon(this.getTextureName() + "_side_9");
+        this.sideIcons[10] = iconRegistry.registerIcon(this.getTextureName() + "_side_10");
+        this.sideIcons[11] = iconRegistry.registerIcon(this.getTextureName() + "_side_11");
+        this.sideIcons[12] = iconRegistry.registerIcon(this.getTextureName() + "_side_12");
+        this.sideIcons[13] = iconRegistry.registerIcon(this.getTextureName() + "_side_13");
+        this.sideIcons[14] = iconRegistry.registerIcon(this.getTextureName() + "_side_14");
+        this.topIcon = iconRegistry.registerIcon(this.getTextureName() + "_top");
     }
 }
--- src/main/java/zeroxstudios/bqenergyexpansion/blocks/EnergySubmissionStation/ESSTileEntity.java
+++ src/main/java/zeroxstudios/bqenergyexpansion/blocks/EnergySubmissionStation/ESSTileEntity.java
@@ -1,7 +1,61 @@
 package zeroxstudios.bqenergyexpansion.blocks.EnergySubmissionStation;
 
+import net.minecraft.block.Block;
+import net.minecraftforge.common.util.ForgeDirection;
 import zeroxstudios.bqenergyexpansion.blocks.base.TileEntityQuest;
 
 public class ESSTileEntity extends TileEntityQuest {
-    public double internalEUStorage;
+
+    public ESSTileEntity() {}
+
+    public double injectEnergy__unused(ForgeDirection directionFrom, double amount, double voltage) {
+        long state = Math.round((this.getEnergyStored() / this.getCapacity()) * 100);
+        Block tmp = worldObj.getBlock(xCoord, yCoord, zCoord);
+        if (tmp instanceof ESSBlock) {
+
+            ESSBlock block = (ESSBlock) tmp;
+            boolean changed;
+
+            if (state < 7) {
+                changed = block.setIcon(0);
+            } else if (state < 14) {
+                changed = block.setIcon(1);
+            } else if (state < 21) {
+                changed = block.setIcon(2);
+            } else if (state < 28) {
+                changed = block.setIcon(3);
+            } else if (state < 35) {
+                changed = block.setIcon(4);
+            } else if (state < 42) {
+                changed = block.setIcon(5);
+            } else if (state < 49) {
+                changed = block.setIcon(6);
+            } else if (state < 56) {
+                changed = block.setIcon(7);
+            } else if (state < 63) {
+                changed = block.setIcon(8);
+            } else if (state < 70) {
+                changed = block.setIcon(9);
+            } else if (state < 77) {
+                changed = block.setIcon(10);
+            } else if (state < 84) {
+                changed = block.setIcon(11);
+            } else if (state < 91) {
+                changed = block.setIcon(12);
+            } else if (state <= 99) {
+                changed = block.setIcon(13);
+            } else {
+                changed = block.setIcon(14);
+            }
+
+            if (changed) {
+                // ToDO: Replace with write/read NBT data and store the icon as nbtdata/metadata
+                worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 0, 3);
+                worldObj.scheduleBlockUpdate(xCoord, yCoord, zCoord, worldObj.getBlock(xCoord, yCoord, zCoord), 1);
+                worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
+            }
+        }
+
+        return super.injectEnergy(directionFrom, amount, voltage);
+    }
 }
